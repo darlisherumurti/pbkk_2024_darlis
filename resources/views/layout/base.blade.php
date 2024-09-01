28a00d618a1be507fd9013bbf61536efde6ba643
ascndia <fixgmc@gmail.com>
2024-09-01 16:02:35 +0700

    update gitignore & hide menu item

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -6,14 +6,14 @@
         <x-menu-item title="List Buku" icon="fas fa-list" :href="route('crud-buku.index')" :active="request()->routeIs('crud-buku.index')">
         </x-menu-item>
 
-        <x-menu-item title="Tambah Buku" icon="fas fa-plus-circle" :href="route('crud-buku.create')" :active="request()->routeIs('crud-buku.create')">
-        </x-menu-item>
+        {{-- <x-menu-item title="Tambah Buku" icon="fas fa-plus-circle" :href="route('crud-buku.create')" :active="request()->routeIs('crud-buku.create')">
+        </x-menu-item> --}}
 
         <x-menu-item title="List Kategori" icon="fas fa-list" :href="route('crud-kategori.index')" :active="request()->routeIs('crud-kategori.index')">
         </x-menu-item>
-
+        {{-- 
         <x-menu-item title="Tambah Kategori" icon="fas fa-plus-circle" :href="route('crud-kategori.create')" :active="request()->routeIs('crud-kategori.create')">
-        </x-menu-item>
+        </x-menu-item> --}}
     </x-menu-tree>
     {{-- <x-menu-item title="Adminlte" icon="fas fa-plus-circle" href="/adminlte/index.html" active=''>
     </x-menu-item> --}}
